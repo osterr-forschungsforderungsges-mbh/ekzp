f73de0fdafa0d24bde6476df47df60581ec2658f
Stefan Stidl <stefan.stidl@ffg.at>
2023-03-03 12:37:20 +0100

    oenace checker demo dyn whitelist/blacklist

--- oenace-check.js
+++ oenace-check.js
@@ -1,18 +1,47 @@
-var oenace3digits;
-
-fetch('https://osterr-forschungsforderungsges-mbh.github.io/ekzp/oenace-3steller.json')
+var oenace3digits = {};
+fetch('oenace-3steller.json')
     .then((response) => response.json())
     .then((json) => {
         oenace3digits = json;
         console.log(json);
     });
 
+var blacklist = [];
+fetch('oenace-blacklist')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}, url: ${response.url}`);
+        }
+        return response.text();
+    })
+    .then((lines) => {
+        blacklist = lines.replaceAll("\r", "").split("\n");
+        console.log(blacklist);
+    });
+
+var whitelist = [];
+fetch('oenace-whitelist')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}, url: ${response.url}`);
+        }
+        return response.text();
+    })
+    .then((lines) => {
+        whitelist = lines.replaceAll("\r", "").split("\n");
+        console.log(whitelist);
+    });
+
+
 
 function allowedOenace(input) {
-    const blacklist = ['10', '02', '03']; // TODO in ressourcen auslagern
-    const whitelist = ['101', '022', '123'];
-    if (input.length == 0) return null;
+    if (input.length == 0 || parseInt(input) == 0) return null;
+    if (blacklist.length == 0 || whitelist.length == 0 || Object.keys(oenace3digits).length == 0) {
+        console.log(`validierungsdaten unvollständig. blacklist: ${blacklist.length}, whitelist: ${whitelist.length}, oenace3digits: ${Object.keys(oenace3digits).length}`);
+        return null;
+    }
     if (!parseInt(input) || isNaN(input) || !input.match(/^[0-9]+$/)) return false;
+    if (input.length >= 3 && oenace3digits[input.substring(0, 3)] === undefined) return false;
     if (input.length < 5) return null;
     if (input.length > 5) return false;
     if (blacklist.includes(input.substring(0, 2))) {
